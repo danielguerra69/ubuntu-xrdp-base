3d4a19245234a1f1092455b5de3e5649e3ce4d69
danielguerra69 <daniel.guerra69@gmail.com>
2017-10-23 19:24:06 +0200

    entrypoint fix
    Check for /etc/entrypoint.d/ before copying the data

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # prepare the etc dir
-cp -R /etc_entrypoint/* /etc/
+if [ ! -f "/etc/entrypoint.d" ];
+then
+		cp -R /etc_entrypoint/* /etc/
+fi
 
 # read entrypoint.d configuration
 for x in `find /etc/entrypoint.d -name '*.conf' | sort`;
